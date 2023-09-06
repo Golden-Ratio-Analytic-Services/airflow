990eec068ef97df91f44931c18b1b454917359d2
ThomasThelen <tommythelen@gmail.com>
2023-09-05 18:23:07 -0700

    Only grab neo4j connections when needed, fix relationship type in road network creation query, use the bolt protocol when connecting to neo4j

--- dags/process-highways.py
+++ dags/process-highways.py
@@ -20,7 +20,7 @@ def process_roadways_dag():
         :return: None
         """
         logging.info("Downloading Mexico road networks...")
-        mexico_graph = ox.graph_from_place({'country': 'Mexico'})
+        mexico_graph = ox.graph_from_place({"country": "Mexico"})
         gdf_nodes, gdf_relationships = ox.graph_to_gdfs(mexico_graph)
         gdf_nodes.reset_index(inplace=True)
         gdf_relationships.reset_index(inplace=True)
@@ -30,7 +30,7 @@ def process_roadways_dag():
         logging.info("Finished saving road networks to disk")
 
     @task
-    def create_indicies(db_session: neo4j.Session) -> None:
+    def create_indices() -> None:
         """
         Creates indicies for faster lookup
 
@@ -38,15 +38,20 @@ def process_roadways_dag():
         :return: None
         """
         logging.info("Creating road network indices")
+        db_session = get_neo4j_connection()
         constraint_query = "CREATE CONSTRAINT IF NOT EXISTS FOR (i:Intersection) REQUIRE i.osmid IS UNIQUE"
-        rel_index_query = "CREATE INDEX IF NOT EXISTS FOR ()-[r:ROAD_SEGMENT]-() ON r.osmids"
-        point_index_query = "CREATE POINT INDEX IF NOT EXISTS FOR (i:Intersection) ON i.location"
+        rel_index_query = (
+            "CREATE INDEX IF NOT EXISTS FOR ()-[r:ROAD_SEGMENT]-() ON r.osmids"
+        )
+        point_index_query = (
+            "CREATE POINT INDEX IF NOT EXISTS FOR (i:Intersection) ON i.location"
+        )
         constraints = [constraint_query, rel_index_query, point_index_query]
         for constraint in constraints:
             db_session.run(constraint)
 
     @task
-    def load_intersections(db_session: neo4j.Session) -> None:
+    def load_intersections() -> None:
         """
         Loads all intersections from an OpenStreetMaps data file
 
@@ -54,17 +59,18 @@ def process_roadways_dag():
         :return:
         """
         logging.info("Loading intersection data into neo4j")
-        load_intersection_query = '''
+        db_session = get_neo4j_connection()
+        load_intersection_query = """
         LOAD CSV WITH HEADERS FROM 'file:///mexico_nodes.csv' AS road
         CALL {
           WITH road
           CREATE(intersection: Intersection{location: point({latitude: toFloat(road.y), longitude: toFloat(road.x)}), osmid: road.osmid})
         } IN TRANSACTIONS OF 1000 ROWS;
-        '''
+        """
         db_session.run(load_intersection_query)
 
     @task
-    def load_intersection_relations(db_session: neo4j.Session) -> None:
+    def load_intersection_relations() -> None:
         """
         Load the roads into neo4j
 
@@ -72,12 +78,13 @@ def process_roadways_dag():
         :return: None
         """
         logging.info("Loading road network connections into neo4j")
-        load_intersection_relation_query = '''
+        db_session = get_neo4j_connection()
+        load_intersection_relation_query = """
         LOAD CSV WITH HEADERS FROM 'file:///mexico_relations.csv' AS segment
         CALL {
           WITH segment
-          MATCH(intersection_a:Intersection {osmid: segment.osmid})
-          MATCH(intersection_b: Intersection {osmid: segment.osmid})
+          MATCH(intersection_a:Intersection {osmid: segment.u})
+          MATCH(intersection_b: Intersection {osmid: segment.v})
           MERGE (intersection_a)-[road_segment:ROAD_SEGMENT {osmid: segment.osmid}]->(intersection_b)
             SET road_segment.oneway = segment.oneway,
             road_segment.ref = segment.ref,
@@ -86,32 +93,47 @@ def process_roadways_dag():
             road_segment.max_speed = segment.maxspeed,
             road_segment.length = toFloat(segment.length)
         } IN TRANSACTIONS OF 1000 ROWS;
-        '''
+        """
         db_session.run(load_intersection_relation_query)
 
-    with neo4j.GraphDatabase.driver("bolt://localhost:7687", auth=('neo4j', 'neo4j222')) as driver:
-        with driver.session(database="neo4j") as session:
-            try:
-                download_map_data()
-                node_file_exists = FileSensor(
-                    task_id='wait_for_node_file',
-                    filepath='/opt/airflow/data/mexico_nodes.csv',
-                    mode='poke',
-                    timeout=60 * 60 * 5,  # Timeout after 5 hours
-                    poke_interval=60,  # Check for the data every minute
-                )
-                relations_file_exists = FileSensor(
-                    task_id='wait_for_network_files',
-                    filepath='/opt/airflow/data/mexico_relations.csv',
-                    mode='poke',
-                    timeout=60 * 60 * 5,  # Timeout after 5 hours
-                    poke_interval=60,  # Check for the data every minute
-                )
-                [relations_file_exists, node_file_exists] >> create_indicies(session) >> load_intersections(
-                    session) >> load_intersection_relations(session)
-            finally:
-                session.close()
-                logging.info("Finished handling road network data")
+    def get_neo4j_connection() -> neo4j.Session:
+        """
+        Gets a session to neo4j
+        """
+        driver = neo4j.GraphDatabase.driver(
+            "bolt://neo4j:7687", auth=("neo4j", "neo4j222")
+        )
+        try:
+            driver.verify_connectivity()
+            session = driver.session(database="neo4j")
+            return session
+        except Exception as connection_error:
+            logging.error("Failed to establish session to neo4j", connection_error)
+            driver.close()
+            session.close()
+
+    download_map_data()
+    node_file_exists = FileSensor(
+        task_id="wait_for_node_file",
+        filepath="/opt/airflow/data/mexico_nodes.csv",
+        mode="poke",
+        timeout=60 * 60 * 5,  # Timeout after 5 hours
+        poke_interval=60,  # Check for the data every minute
+    )
+    relations_file_exists = FileSensor(
+        task_id="wait_for_network_files",
+        filepath="/opt/airflow/data/mexico_relations.csv",
+        mode="poke",
+        timeout=60 * 60 * 5,  # Timeout after 5 hours
+        poke_interval=60,  # Check for the data every minute
+    )
+    (
+        [relations_file_exists, node_file_exists]
+        >> create_indices()
+        >> load_intersections()
+        >> load_intersection_relations()
+    )
+    logging.info("Finished handling road network data")
 
 
 process_roadways_dag()
